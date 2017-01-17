910775572ec0c3720f2d231c17ef1251fa7f6f69
Nathanael Farley <nasfarley88@gmail.com>
2017-01-17 17:28:40 +0000

    Cleaned up code.

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ async def get_key():
 BOT_JSON = None
 
 
-async def get_me(cls):
+async def get_me():
     global BOT_JSON
     if not BOT_JSON:
         async with aiohttp.ClientSession() as session:
@@ -41,43 +41,14 @@ async def is_being_thanked(bot, msg):
         pass
 
 
-class ThankYouBeardHelper(BeardChatHandler):
-
-    _get_me = None
-
-    _timeout = 1
-
-    @classmethod
-    async def get_me(cls):
-        if not cls._get_me:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(
-                        "https://api.telegram.org/bot{}/getMe".format(
-                            BeardChatHandler.key)) as resp:
-                    cls._get_me = (await resp.json())['result']
-
-        return cls._get_me
-
-    @classmethod
-    async def is_being_thanked(cls, bot, msg):
-        thank_yous = ['thank', 'thanks', 'thx']
-        me = await cls.get_me()
-        print(me)
-        try:
-            if any((re.match(r"\b{}\b".format(x), msg['text'].lower()) for x in thank_yous)) and\
-               me['first_name'] in msg['text']:
-                return True
-        except AttributeError:
-            pass
-
-
 class ThankYouBeard(BeardChatHandler):
 
     __userhelp__ = """A simple echo beard. Echos whatever it is sent."""
 
     __commands__ = [
-        (ThankYouBeardHelper.is_being_thanked, 'say_thank_you', None)
+        (is_being_thanked, 'say_thank_you', None)
     ]
 
     async def say_thank_you(self, msg):
-        await self.sender.sendMessage("No problem, {}.".format(msg['from']['first_name']))
+        await self.sender.sendMessage(
+            "No problem, {}.".format(msg['from']['first_name']))
